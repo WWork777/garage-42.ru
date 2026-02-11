3eb0c7fce3cac1affcc43f767ef39fb2846c2d79
id0ntc4re <parsikovevgenij470@gmail.com>
2026-02-11 22:16:02 +0700

    qwe

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
+import Script from "next/script";
 import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "./components/layout/footer/Footer";
 import Header from "./components/layout/header/Header";
-import YandexMetrika from "./components/YandexMetrika/YandexMetrika";
 
 const geistSans = localFont({
   src: [
@@ -22,14 +22,43 @@ const geistSans = localFont({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <head>
-        <YandexMetrika />
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(106779809, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              });
+            `,
+          }}
+        />
       </head>
-      <body className={`${geistSans.variable}`}>
+
+      <body className={geistSans.variable}>
         <Header />
         {children}
         <Footer />
+
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/106779809"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
